c0fc7581a59dd549cf2d7ab5bf71c65408bbf96c
Martin Smolka <martinsmolka@Martins-MacBook-Pro-2.local>
2019-10-13 05:38:36 -0400

    work

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SnackbarService } from '../../services/snackbar.service';
+import { PartyService } from '../../services/party.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +17,8 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private snackbarService: SnackbarService,
+    private partyService: PartyService,
+    private router: Router,
     ) { }
 
   ngOnInit() {
@@ -26,12 +30,18 @@ export class HomeComponent implements OnInit {
   async onSubmit() {
     try {
       this.loading = true;
-    } catch ({ message = 'Error authentication, please try again' }) {
-      this.loginError = message;
-      this.snackbarService.showError(message, 'Close');
+      this.partyService.setParty(this.loginForm.getRawValue().keycode);
+      console.log(this.loginForm.getRawValue().keycode);
+      await this.partyService.getParty();
+      this.router.navigate(['party']);
+    } catch (err) {
+      this.snackbarService.showError('Party does not exist', 'close');
     } finally {
       this.loading = false;
     }
+  }
+  hosting() {
+    this.router.navigate(['/planners']);
   }
 
 }
